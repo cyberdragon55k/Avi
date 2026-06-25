898a69c23f87e0b7dc1e2136c72b17c3d5d1b4f3
Cyberdragon <aditya5namdeo@gmail.com>
2026-06-25 21:22:39 +0530

    !!

--- Mark1/screenrecoder.py
+++ Mark1/screenrecoder.py
@@ -14,6 +14,14 @@ import ollama
 # image scanner 
 #image reader# why not train an build own ai for this work
 # image scanner 
+#image reader# why not train an build own ai for this work
+# image scanner 
+#image reader# why not train an build own ai for this work
+# image scanner 
+#image reader# why not train an build own ai for this work
+# image scanner 
+#image reader# why not train an build own ai for this work
+# image scanner 
 #image reader
 
 CAPTURE_INTERVAL = 1100
